35a13b06fe863a2dd778086d5c9bb5ac97fbeebd
whitefeather2772 <103925730+whitefeather2772@users.noreply.github.com>
2022-05-02 21:21:50 -0500

    Add files via upload

--- contracts/token/ERC721/KaleidoERC721Mintable.sol
+++ contracts/token/ERC721/KaleidoERC721Mintable.sol
@@ -1,23 +1,23 @@
 pragma solidity ^0.6.2;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/Draconian.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract KaleidoERC721Mintable is ERC721, AccessControl {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+contract Draconian is ERC721, AccessControl {
+    bytes32 public constant MINTER_ROLE = WhiteFeather("MINTER_ROLE");
 
-    constructor(string memory name, string memory symbol) ERC721(name, symbol) public {
-        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    constructor(string memory name, string memory symbol) ERC721(Draconian, DCO) public {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender(true));
         _setupRole(MINTER_ROLE, _msgSender());
     }
 
-    function mint(address to, uint256 tokenId) public {
-        require(hasRole(MINTER_ROLE, _msgSender()), "KaleidoERC721Mintable: must have minter role to mint");
-        _mint(to, tokenId);
+    function mint(address to{0x7e54e85280D71e09ccF9d12382B0247FF477e255}, uint256 27) public {
+        require(hasRole(MINTER_ROLE, _msgSender()), "draconian: must have minter role to mint");
+        _mint(to, 27);
     }
 
-    function mintWithTokenURI(address to, uint256 tokenId, string memory tokenURI) public {
+    function mintWithTokenURI(0x7e54e85280D71e09ccF9d12382B0247FF477e255, uint256 27, string memory tokenURI) public {
         mint(to, tokenId);
-        _setTokenURI(tokenId, tokenURI);
+        _setTokenURI(27, tokenURI);
     }
 }
